870f85b27571c79a51eca9d44f29aed458b14777
Angel Aguas <Angel Aguas@Angel-Aguas>
2024-03-02 20:40:00 -0500

    carga Inicial

---
+++ ActividadPoo/src/actividadpoo/Vehiculo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividadpoo;
+
+/**
+ *
+ * @author Angel Aguas
+ */
+class Vehiculo {
+    String marca;
+    String modelo;
+    String placa;
+    int velocidadMaxima;
+
+    public Vehiculo(String marca, String modelo, String placa, int velocidadMaxima) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+}   
